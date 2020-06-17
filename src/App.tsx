02d6aa6ad8cd8ccd0dccce74021793bb5d1aa4b3
David Blanar <david.blanar@gmail.com>
2020-06-17 17:49:10 +0200

    message displaying

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,14 @@ import "./App.css";
 import { data } from "./data";
 import Message from "./Message";
 
+// TODO test on browsers
+
 const App = () => {
   return (
     <div className="App">
       <div className="chat-window">
-        {data.map((item) => (
+        {/* TODO remove */}
+        {data.slice(0, 10).map((item) => (
           <Message message={item} key={item.id} />
         ))}
       </div>
--- src/Message.tsx
+++ src/Message.tsx
@@ -1,12 +1,54 @@
 import React from "react";
+import { format } from "date-fns";
 import { IMessage } from "./interfaces";
 
 interface Props {
   message: IMessage;
 }
 
+const checkmark = String.fromCharCode(10003);
+
 const Message: React.FC<Props> = ({ message }) => {
-  return <div className="message">{message.text}</div>;
+  const renderCheckmarks = () => {
+    switch (message.status) {
+      case "sent":
+        return <span>{checkmark}</span>;
+      case "received":
+        return (
+          <span>
+            {checkmark}
+            {checkmark}
+          </span>
+        );
+      case "read":
+        return (
+          <span className="text-blue-500">
+            {checkmark}
+            {checkmark}
+          </span>
+        );
+      default:
+        return null;
+    }
+  };
+
+  const date = new Date(parseInt(message.timestamp) * 1000);
+  const formattedDate = format(date, "dd MMM yyyy - HH:mm:ss");
+  return (
+    <div
+      className={`flex ${
+        message.direction === "in" ? "justify-start" : "justify-end"
+      }`}
+    >
+      <div className="w-3/4 p-2 mt-4 border-solid border border-gray-600 rounded-md">
+        <p className="text-gray-600 text-xs">{formattedDate}</p>
+        <p className="text-sm font-bold flex justify-between">
+          {message.text}
+          {message.direction === "out" && renderCheckmarks()}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Message;
