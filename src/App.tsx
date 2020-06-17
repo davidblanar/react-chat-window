293166d470d4a5315c0dd4e675560ccc4f218909
David Blanar <david.blanar@gmail.com>
2020-06-17 16:59:25 +0200

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import React from 'react';
+import React from "react";
 import "./tailwind.generated.css";
-import './App.css';
+import "./App.css";
 
 const App = () => {
-  return (
-    <div className="App">
-
-    </div>
-  );
-}
+  return <div className="App"></div>;
+};
 
 export default App;
